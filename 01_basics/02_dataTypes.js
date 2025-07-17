616bde44420295968f86903e0b3cee15dbeff4a5
Saurabh Singh <sonubisht291@gmail.com>
2025-07-17 08:42:58 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all Js code as newer version 
+
+// alert (3+3) we are using nodejs, not browser
+
+console.log (3
++3
+) // code readability should be high
+
+// console.log("Saurabh Singh");
+
+let name = "saurabh Singh"
+let age = 22
+let loggedIn = true
+
+// number = 251565
+// bigInt = big intiger(number)
+// string = "" 
+// boolean = true/false
+// null = standlone value  (empty value)
+// undefined = Not assinges value      (when we set a variable but not give value of this)
+// symbol = unique
+
+
+
+// object 
+console.log(typeof null);  // null is show object
+console.log(typeof undefined); // undefined 

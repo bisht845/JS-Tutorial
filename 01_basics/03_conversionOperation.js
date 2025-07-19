5967a7d08b1c7348ebb8f80cbbff2a5b2be9d61f
Saurabh Singh <sonubisht291@gmail.com>
2025-07-19 07:48:47 +0000

    operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = "saurabh"
 
 let booleanIsLoeggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoeggedIn);
+// console.log(booleanIsLoeggedIn);
 
 
 // 1 = convert into true ;
@@ -27,9 +27,39 @@ let sumNumber = 33
 
 let stringNumber = String(sumNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
 
-// we learn how to convert value in string number boolean
+// we learn how to convert value in string number boolean^^
+
+
+// *************************** Operations *******************************
+
+let value = 3 
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Hello"
+let str2 = " Saurabh"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+// if string first then all value treat in sting 
+// if string is last then first value will conversion or add 

b9922facd213ba201988289fa085ab92259ece66
Saurabh Singh <sonubisht291@gmail.com>
2025-07-19 07:07:34 +0000

    conversion of data types

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = "Boolean"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" = 33 (string is easly convert into number)
+// "33abc" = Not converted value come = NaN (Not a number)
+// true  = converted in 1(true = 1 ) & false is 0 (false = 0)
+
+let isLoggedIn = "saurabh"
+
+let booleanIsLoeggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoeggedIn);
+
+
+// 1 = convert into true ;
+// "" = false 
+// "saurav" = true 
+
+
+let sumNumber = 33 
+
+let stringNumber = String(sumNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
+// we learn how to convert value in string number boolean
